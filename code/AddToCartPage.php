4657beb0c88fef6e25f93c015a86eeae48165b75
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-07-16 01:12:30 +0000

    complete makeover of ecommerce_quick_add module
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_quick_add/trunk@3032 950b781c-c544-efab-2f99-90083ee69683

--- code/AddToCartPage.php
+++ code/AddToCartPage.php
@@ -18,25 +18,6 @@ class AddToCartPage extends Page {
 		"ExcludedProductsAndGroups" => "SiteTree",
 	);
 
-	public function ProductsAndGroupsToBeExcludedArray() {
-		$components = $this->getManyManyComponents('ExcludedProductsAndGroups');
-		if($components && $components->count()) {
-			return $components->column("ID");
-		}
-		else {
-			return array(0 => 0);
-		}
-	}
-
-	public function ProductsAndGroupsToBeExcludedWhereString() {
-		$array = $this->ProductsAndGroupsToBeExcludedArray();
-		$stage = '';
-		if(Versioned::current_stage() == "Live") {
-			$stage = "_Live";
-		}
-		return "\"SiteTree$stage\".\"ID\" NOT IN (".implode(",", $array).")";
-	}
-
 
 }
 
@@ -47,24 +28,85 @@ class AddToCartPage_Controller extends Page_Controller {
 		parent::init();
 		Requirements::themedCSS('Products');
 		Requirements::themedCSS('AddToCartPage');
+		Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
+		//Requirements::block(THIRDPARTY_DIR."/jquery/jquery.js");
+		//Requirements::javascript(Director::protocol()."ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
+		Requirements::javascript(THIRDPARTY_DIR."/jquery-form/jquery.form.js");
 		Requirements::javascript("ecommerce_quick_add/javascript/AddToCartPage.js");
 	}
 
-	function TopLevelGroups() {
-		if($dos = DataObject::get("ProductGroup", "\"ParentID\" = 0 AND ".$this->ProductsAndGroupsToBeExcludedWhereString())) {
-			return $dos;
+	function AddMemberToCartForm(){
+		$member = Member::currentMember();
+		$order = ShoppingCart::current_order();
+		$currentCustomer = $order->CreateOrReturnExistingMember(false);
+		if($member && $member->IsShopAdmin()) {
+			$fields = new FieldSet(
+				new DropdownField("MemberID", _t("AddToCartPage.CUSTOMER", "Customer"), EcommerceRole::list_of_customers()),
+				new ReadonlyField("CurrentMember", _t("AddToCartPage.CURRENTCUSTOMER", "Current Customer"), $currentCustomer->getTitle())
+			);
+			$actions = new FieldSet(
+				new FormAction("addmembertocartform_add", _t("AddToCartPage.ADDMEMBERTOORDER", "Add Customer to current order"))
+			);
+			$validator = new RequiredFields(array("MemberID"));
+			return new Form($this, "AddMemberToCartForm", $fields, $actions, $validator);
 		}
-		else {
-			$stage = '';
-			if(Versioned::current_stage() == "Live") {
-				$stage = "_Live";
-			}
-			if($dos = DataObject::get("ProductGroup", "ParentPage.ParentID = 0 AND ".$this->ProductsAndGroupsToBeExcludedWhereString(), "", "INNER JOIN SiteTree$stage AS ParentPage ON SiteTree$stage.ParentID = ParentPage.ID")) {
-				return $dos;
+	}
+
+	function addmembertocartform_add($data, $form) {
+		$member = Member::currentMember();
+		if($member && $member->IsShopAdmin()) {
+			$order = ShoppingCart::current_order();
+			$member = DataObject::get_by_id("Member", intval($data["MemberID"]));
+			if($member) {
+				$order->MemberID = $member->ID;
+				$order->write();
+				$response = $member->getTitle()." "._t("AddToCartPage.ADDED", "Added");
+				$status = "good";
 			}
 			else {
-				return DataObject::get("ProductGroup", "GrandParentPage.ParentID = 0 AND ".$this->ProductsAndGroupsToBeExcludedWhereString(), "", "INNER JOIN SiteTree$stage AS ParentPage ON SiteTree$stage.ParentID = ParentPage.ID INNER JOIN SiteTree$stage AS GrandParentPage ON ParentPage.ParentID = GrandParentPage.ID");
+				$response = _t("AddToCartPage.CUSTOMERNOTADDED", "Customer could not be added");
+				$status = "bad";
 			}
+			if(Director::is_ajax()) {
+				return $response;
+			}
+			else {
+				$form->setMessage($response, $status);
+				Director::redirectBack();
+			}
+		}
+	}
+
+	function QuickAddToCartForm(){
+		$fields = new FieldSet(
+			new HiddenField("BuyableID"),
+			new HiddenField("BuyableClassName"),
+			new HiddenField("Version"),
+			new BuyableSelectField("FindBuyable", _t("AddToCartPage.PRODUCT", "Product")),
+			new NumericField("Quantity", _t("AddToCartPage.QUANTITY", "Quantity"), 1)
+		);
+		$actions = new FieldSet(
+			new FormAction("quickaddtocartform_add", _t("AddToCartPage.ADD", "Add"))
+		);
+		$validator = new RequiredFields(array("BuyableID", "BuyableClassName", "Version", "Quantity"));
+		return new Form($this, "QuickAddToCartForm", $fields, $actions, $validator);
+	}
+
+	function quickaddtocartform_add($data, $form) {
+		$buyableID = intval($data["BuyableID"]);
+		$buyableClassName = Convert::raw2sql($data["BuyableClassName"]);
+		$version = Intval($data["Version"]);
+		$quantity = floatval($data["Quantity"]);
+		$buyable = DataObject::get_by_id($buyableClassName, $buyableID);
+		$orderItem = $buyable->OrderItem();
+		$orderItem->Quantity = $quantity;
+		$orderItem->Version = $version;
+		$orderItem->write();
+		if(Director::is_ajax()) {
+			return _t("AddToCartPage.ADDED", "Added");
+		}
+		else {
+			Director::redirectBack();
 		}
 	}
 
--- javascript/AddToCartPage.js
+++ javascript/AddToCartPage.js
@@ -1,62 +1,60 @@
 
 jQuery(document).ready(
 	function(){
-		AddToCartPage.init();
+		EcomAddToCartPage.init();
 	}
 );
 
 
-var AddToCartPage = {
+var EcomAddToCartPage = {
 
 	loadingClass: "loading",
-
-	completedClass: "completed",
-
-	ulGroupsSelector: ".ULgroups, .ULproducts, div.productActionsOuter",
-
-	expandLinkSelector: ".ULgroups a.groupLink, .ULproducts a.productLink",
-
-	expandDivSelector: "ul, div.productActionsOuter",
-
-	levelOneSelector: "UL.level1",
-
-	moreInfoHolderSelector: ".moreInfoHolder",
-
-	moreInfoLinkSelector: ".productLink",
+	formSelector: "#QuickAddToCartFormHolder form",
+	actiomButtonSelector: "#QuickAddToCartFormHolder form .Actions input",
+	responseSelector: ".selectedBuyable span",
+	findBuyableFieldSelector: ".findBuyable input",
 
 	init: function(){
-		jQuery(AddToCartPage.ulGroupsSelector).hide();
-		jQuery(AddToCartPage.expandLinkSelector).click(
-			function(event) {
-				event.preventDefault();
-				jQuery(this).next(AddToCartPage.expandDivSelector).slideToggle();
-			}
-		);
-		jQuery(AddToCartPage.levelOneSelector).slideDown();
-		jQuery(AddToCartPage.moreInfoHolderSelector).hide();
-		jQuery(AddToCartPage.moreInfoLinkSelector).click(
-			function(event) {
-				event.preventDefault();
-				var parentElement = this;
-				var domElement = "#" + jQuery(this).attr("rel");
-				var url = jQuery(this).attr("href");
-				if(jQuery(domElement).hasClass(AddToCartPage.completedClass)) {
-					jQuery(domElement).slideToggle();
-				}
-				else {
-					jQuery(parentElement).addClass(AddToCartPage.loadingClass);
-					jQuery.get(
-						url,
-						function(html) {
-							jQuery(domElement).html(html).addClass(AddToCartPage.completedClass).slideDown();
-							jQuery(parentElement).removeClass(AddToCartPage.loadingClass);
-						}
-					);
-				}
-			}
-		);
+		var options = {
+			target:        EcomAddToCartPage.responseSelector,   // target element(s) to be updated with server response
+			beforeSubmit:  EcomAddToCartPage.showRequest,  // pre-submit callback
+			success:       EcomAddToCartPage.showResponse  // post-submit callback
+			// other available options:
+			//url:       url         // override for form's 'action' attribute
+			//type:      type        // 'get' or 'post', override for form's 'method' attribute
+			//dataType:  null        // 'xml', 'script', or 'json' (expected server response type)
+			//clearForm: true        // clear all form fields after successful submit
+			//resetForm: true        // reset the form after successful submit
+			// jQuery.ajax options can be used here too, for example:
+			//timeout:   3000
+		};
+		// bind form using 'ajaxForm'
+		jQuery(EcomAddToCartPage.formSelector).ajaxForm(options);
+	},
+	// pre-submit callback
+	showRequest: function(formData, jqForm, options) {
+		jQuery(EcomAddToCartPage.actiomButtonSelector).hide();
+
+		// here we could return false to prevent the form from being submitted;
+		// returning anything other than false will allow the form submit to continue
+		return true;
+	},
+
+	// post-submit callback
+	showResponse: function(responseText, statusText, xhr, jQueryform)  {
+		jQuery(EcomAddToCartPage.actiomButtonSelector).show();
+		jQuery(EcomAddToCartPage.responseSelector).html(responseText);
+		jQuery(EcomAddToCartPage.findBuyableFieldSelector).focus();
+		// for normal html responses, the first argument to the success callback
+		// is the XMLHttpRequest object's responseText property
+
+		// if the ajaxForm method was passed an Options Object with the dataType
+		// property set to 'xml' then the first argument to the success callback
+		// is the XMLHttpRequest object's responseXML property
+
+		// if the ajaxForm method was passed an Options Object with the dataType
+		// property set to 'json' then the first argument to the success callback
+		// is the json data object returned by the server
+
 	}
-
-
-
 }
