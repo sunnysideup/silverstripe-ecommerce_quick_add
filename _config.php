ead3532d27b1697419d6e72ebbe5134a8f00e40d
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2014-04-09 04:39:55 +0000

    UPGRADE 3.1
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_quick_add/trunk@4926 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -1,11 +1,5 @@
 <?php
 
 /**
- * developed by www.sunnysideup.co.nz
  * author: Nicolaas - modules [at] sunnysideup.co.nz
 **/
-
-
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START ecommerce_quick_add MODULE ----------------===================
-//===================---------------- END ecommerce_quick_add MODULE ----------------===================
